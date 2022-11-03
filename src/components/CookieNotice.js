1f82c28de37fa8645b37361bb889c5708a7c0f28
Alex Attemann <a.attemann@gmail.com>
2022-11-03 21:24:59 +0000

    fix: cookie notice

--- src/components/CookieNotice.js
+++ src/components/CookieNotice.js
@@ -1,59 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import * as styles from "./CookieNotice.module.sass";
 import cookie from "../img/cookie.svg";
 import { document } from "browser-monads";
 
-function checkACookieExists() {
-  if (
+const CookieNotice = () => {
+  const [cookieExists, setCookieExists] = useState(
     document.cookie
       .split(";")
       .some((item) => item.trim().startsWith("acceptCookies="))
-  ) {
-    return true;
-  }
-}
-
-function removeItem(sKey, sPath, sDomain) {
-  document.cookie =
-    encodeURIComponent(sKey) +
-    "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
-    (sDomain ? "; domain=" + sDomain : "") +
-    (sPath ? "; path=" + sPath : "");
-}
-
-const acceptCookies = () => {
-  document.cookie = "acceptCookies=true; SameSite=None; Secure";
-};
-
-const rejectCokies = () => {
-  document.cookie = "acceptCookies=true; SameSite=None; Secure";
-};
-
-let gaProperty = "G-7MVZD0BJZG";
-const disableStr = "ga-disable-" + gaProperty;
-
-const CookieNotice = () => {
-  return (
-    <div className={styles.container}>
-      <img src={cookie} alt="" aria-hidden></img>
-      <h3>We use cookies</h3>
-      <p>
-        Cookies help us deliver the best experience on our website, By using our
-        website, you agree to the use of cookies. Learn more.
-      </p>
-      <Button onClick={() => acceptCookies()} isWide>
-        Accept all cookies
-      </Button>
-      <button
-        ofType="primaryOutline"
-        onClick={() => rejectCokies()}
-        className={styles.rejectBtn}
-      >
-        Use only necessary cookies
-      </button>
-    </div>
+      ? true
+      : false
   );
+
+  const acceptCookies = () => {
+    document.cookie =
+      "acceptCookies=true; SameSite=None; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+    setCookieExists(true);
+  };
+
+  let gaProperty = "G-7MVZD0BJZG";
+  const disableStr = "ga-disable-" + gaProperty;
+
+  const rejectCokies = () => {
+    document.cookie =
+      "acceptCookies=false; SameSite=None; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+    document.cookie =
+      disableStr + "=true; expires=Thu, 31 Dec 2099 23:59:59 UTC;path=/";
+    setCookieExists(true);
+  };
+
+  if (cookieExists === false) {
+    return (
+      <div className={styles.container}>
+        <img src={cookie} alt="" aria-hidden></img>
+        <h3>We use cookies</h3>
+        <p>
+          Cookies help us deliver the best experience on our website, By using
+          our website, you agree to the use of cookies. Learn more.
+        </p>
+        <Button onClick={() => acceptCookies()} isWide>
+          Accept all cookies
+        </Button>
+        <button
+          ofType="primaryOutline"
+          onClick={() => rejectCokies()}
+          className={styles.rejectBtn}
+        >
+          Use only necessary cookies
+        </button>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default CookieNotice;
--- src/components/ScrollUp.js
+++ src/components/ScrollUp.js
@@ -11,12 +11,12 @@ const ScrollUp = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle cx="32" cy="32" r="30" stroke-width="4" />
+        <circle cx="32" cy="32" r="30" strokeWidth="4" />
         <path
           d="M31.6521 43.9423V19.3625M20.8984 30.1162L31.6521 19.3625L42.4057 30.1162"
-          stroke-width="5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </button>
